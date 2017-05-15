6a1a50054c4ef6ad8e6752b1291503b986a2f5c9
kennybuc <kenny.buc88@gmail.com>
2017-05-15 04:28:35 +0200

    initial commit for all project files

---
+++ CustomCamera/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  CustomCamera
+//
+//  Created by admin_user on 5/15/17.
+//  Copyright © 2017 admin_user. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
